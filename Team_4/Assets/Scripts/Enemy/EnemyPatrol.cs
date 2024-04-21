3767c52d22a537e915af67720e07ab5b6eefbbea
MinimalisTy <Rusya.s06@mail.ru>
2024-04-21 19:09:47 +0800

    Enemy AI 0.2
    Улучшил ИИ. Теперь он свободно передвигается по всей карте и не зависит от каких-либо точек. Ещё научился останавливаться в случайный момент во время патрулирования.

--- Team_4/Assets/Scripts/Enemy/EnemyPatrol.cs
+++ Team_4/Assets/Scripts/Enemy/EnemyPatrol.cs
@@ -7,10 +7,10 @@ public class EnemyPatrol : MonoBehaviour
 {
 
     NavMeshAgent agent;
-    public List<GameObject> PatrolPoints;
+    //public List<GameObject> PatrolPoints;
     public Transform Player;
     public float ViewAngle;
-
+    bool Rest = false;
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -22,17 +22,49 @@ public class EnemyPatrol : MonoBehaviour
         EnemySeenPlayer();
     }
 
+
+
+
+
+
     void Patrol()
     {
-        
-        int i = 0;
+        float x = 0;
+        float z = 0;
+        Vector3 pos = Vector3.zero;
         if(agent.remainingDistance <= 2)
         {
-            i = Random.Range(0, PatrolPoints.Count);
-            agent.destination = PatrolPoints[i].transform.position;
+            x = Random.Range(-10, 10);
+            z = Random.Range(-10, 10);
+            pos = new Vector3(x, 0, z);
+            agent.destination = gameObject.transform.position + pos;
         }
-
+        if (!Rest && !agent.isStopped)
+        {
+            float timeToRest = Random.Range(10, 20);
+            Invoke("RestOnPatrol", timeToRest);
+            Rest = true;
+        }
+            
     }
+    void RestOnPatrol()
+    {
+        float timeForRest = Random.Range(5, 10);
+        Rest = false;
+        agent.Stop();
+        Invoke("ReturnToPatrol", timeForRest);
+    }
+    void ReturnToPatrol()
+    {
+        agent.Resume();
+    }
+
+
+
+
+
+
+
 
     void EnemySeenPlayer()
     {
