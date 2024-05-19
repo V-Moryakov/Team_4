9a48684b814fd8481e1f096fd9e4501ae62ee529
V-Moryakov <79987852+V-Moryakov@users.noreply.github.com>
2024-05-20 01:07:25 +0300

    Дорисовал обе сцены

---
+++ Team_4/Assets/Scripts/LoaderPointToBossScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoaderPointToBossScene : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        var player = other.gameObject.GetComponent<PlayerController>();
+        if (player != null)
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
