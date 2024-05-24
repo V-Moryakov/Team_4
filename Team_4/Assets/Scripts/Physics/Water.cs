cf2dec735b1d9b344d1b064007218bf8cefe5262
MinimalisTy <Rusya.s06@mail.ru>
2024-05-24 18:53:35 +0800

    персонаж "тонет"(получает урон в воде)

---
+++ Team_4/Assets/Scripts/Physics/Water.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<PlayerHealth>())
+        {
+            other.GetComponent<PlayerHealth>().DealDamage(10);
+        }
+    }
+
+}
