52b2db190d20b547ca9a36bd6c20d05f306a8590
MrsTengo <161305601+MrsTengo@users.noreply.github.com>
2024-05-24 20:53:19 +0300

    Исправила ошибку в туториале

--- Team_4/Assets/UI/Scripts/TutorialManager.cs
+++ Team_4/Assets/UI/Scripts/TutorialManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TutorialManager : MonoBehaviour
 {
@@ -14,7 +15,10 @@ public class TutorialManager : MonoBehaviour
     void Start()
     {
         sentences = new Queue<string>();
-        StartDialogue();
+        if (SceneManager.sceneCountInBuildSettings == 1)
+        {
+            StartDialogue();
+        }
         //message[0] = "Здравствуй колдунья! Тебе предстоит зачистить три лагеря, добывая с врагов необходимые для зелий ингредиенты или собирая их в лесу (клавиша E).";
         //message[1] = "Поначалу будет сложнл, но чтобы облегить свой путь, используй ctrl, что позволит тебе подкрадываться к врагам незаметно.";
         //message[2] = "Ты повысишь свои шансы на успех, создавая разнообразные зелья в инвенатре (клавиша I). Чтобы создать их, используй рецепты, которые откроются тебе по мере прохождения.";
