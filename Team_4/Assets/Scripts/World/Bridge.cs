5237d96bfd628f47acd6fc50f21f58ba547f501f
MinimalisTy <Rusya.s06@mail.ru>
2024-05-24 21:57:38 +0800

    мост

---
+++ Team_4/Assets/Scripts/World/Bridge.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge : MonoBehaviour
+{
+
+    Animator anim;
+
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        KnightIsFall();
+    }
+
+
+    void KnightIsFall()
+    {
+        if (!FindObjectOfType<Knight>() && !anim.GetBool("KnightIsFall"))
+        {
+            GetComponent<Animator>().enabled = true;
+            anim.SetBool("KnightIsFall", true);
+            Invoke("Pos", 2);
+        }
+        if(FindObjectOfType<Knight>())
+        {
+            gameObject.transform.position = new Vector3(160.29f, -10.9f, 166.2f);
+            anim.SetBool("KnightIsFall", false);
+        }
+    }
+
+    void Pos()
+    {
+        GetComponent<Animator>().enabled = false;
+        gameObject.transform.position = new Vector3(160.29f, -5.71f, 166.2f);
+    }
+
+}
